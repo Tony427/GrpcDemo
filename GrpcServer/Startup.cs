5687871bf46ea231dcf7b28a02b22a5647082734
TonyTsai <tony@unifiedpractice.com>
2021-02-24 19:53:26 +0800

    fix namespace & add customers.proto in client project

--- GrpcServer/Startup.cs
+++ GrpcServer/Startup.cs
@@ -1,5 +1,4 @@
-﻿using GrpcServer.Services;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;

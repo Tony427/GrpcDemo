2be280901d4d296413ccf9bbe6680b4d624d8025
TonyTsai <tony@unifiedpractice.com>
2021-02-24 19:08:36 +0800

    add client side code

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Tony" });
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
         }
     }
 }

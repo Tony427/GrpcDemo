df8be05e2b21caf3bac9e0c56657290359469364
TonyTsai <tony@unifiedpractice.com>
2021-02-24 19:50:53 +0800

    register Customer in Startup.cs

--- GrpcServer/Services/CustomersService.cs
+++ GrpcServer/Services/CustomersService.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GrpcServer.Services
+namespace GrpcServer
 {
     public class CustomersService : Customer.CustomerBase
     {

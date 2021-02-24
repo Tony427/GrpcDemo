ea44346a9b95ca396add0a38c85ff8aca954383e
TonyTsai <tony@unifiedpractice.com>
2021-02-24 19:55:22 +0800

    apply customer client code

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -16,6 +16,13 @@ namespace GrpcClient
 
             //Console.WriteLine(reply.Message);
 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
+
+            var reply = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1 });
+
+            Console.WriteLine(reply);
+
             Console.ReadLine();
         }
     }

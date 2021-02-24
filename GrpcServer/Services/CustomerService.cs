95e9283bfa3ab5b5cca7cf43a8902e0462d327e5
TonyTsai <tony@unifiedpractice.com>
2021-02-24 19:35:36 +0800

    add CustomerService and fix typo

---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            return base.GetCustomerInfo(request, context);
+        }
+    }
+}
